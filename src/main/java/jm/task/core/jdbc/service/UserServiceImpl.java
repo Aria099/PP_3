02bed8dcdae8d043983d40084b1613549c28f630
Aria <aria_099@mail.ru>
2025-02-01 21:08:39 +0300

    First commit

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    UserDao userDao = new UserDaoJDBCImpl();;
+    UserDao userDao = new UserDaoJDBCImpl();
 
     public UserServiceImpl() { }
 
